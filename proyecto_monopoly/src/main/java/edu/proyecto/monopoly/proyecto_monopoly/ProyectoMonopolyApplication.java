0e2139574e7ff811e2261f2426806993138a23a7
Alex Garcia <alexgarciagandia304@gmail.com>
2024-02-02 12:54:19 +0100

    Base Spring Boot

---
+++ proyecto_monopoly/src/main/java/edu/proyecto/monopoly/proyecto_monopoly/ProyectoMonopolyApplication.java
@@ -0,0 +1,13 @@
+package edu.proyecto.monopoly.proyecto_monopoly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoMonopolyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoMonopolyApplication.class, args);
+	}
+
+}
---
+++ proyecto_monopoly/src/test/java/edu/proyecto/monopoly/proyecto_monopoly/ProyectoMonopolyApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.proyecto.monopoly.proyecto_monopoly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoMonopolyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
