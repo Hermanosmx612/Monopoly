aaff044612711a194c64ba07d5743da2176dbbae
Alex Garcia <alexgarciagandia304@gmail.com>
2024-02-21 12:02:48 +0100

    Corregido error

--- proyecto_monopoly/src/main/java/edu/proyecto/monopoly/proyecto_monopoly/repository/PropietarioRepository.java
+++ proyecto_monopoly/src/main/java/edu/proyecto/monopoly/proyecto_monopoly/repository/PropietarioRepository.java
@@ -13,7 +13,7 @@ import edu.proyecto.monopoly.proyecto_monopoly.model.db.PropietarioDb;
 public interface PropietarioRepository extends JpaRepository<PropietarioDb, Integer>{
     @Query("SELECT CASE WHEN COUNT(p) > 0 THEN true ELSE false END FROM PropietarioDb p WHERE p.partida.id = :idPartida AND p.casilla.id = :idCasilla")
     boolean existsByPartidaIdAndCasillaId(@Param("idPartida") int idPartida, @Param("idCasilla") int idCasilla);
-    @Query("SELECT FROM PropietarioDb p WHERE p.jugador.id = :p.jugador.id AND p.casilla.id = :idCasilla AND p.partida.id = :idPartida")
+    @Query("SELECT p FROM PropietarioDb p WHERE p.jugador.id = :idJugador AND p.casilla.id = :idCasilla AND p.partida.id = :idPartida")
     Optional<PropietarioDb> getUserIdPropietario(@Param("idJugador") int idJugador, @Param("idCasilla") int idCasilla, @Param("idPartida") int idPartida);
     
 }
